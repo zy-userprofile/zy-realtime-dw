16c1cd4d5fc94f06afbe1352db019cecb2f2c99d
hufenggang <michael.hu@jollycorp.com>
2019-12-13 16:17:27 +0800

    build dao model

---
+++ realtime-dw-dao/src/main/java/cn/zy/userprofile/realtime/dw/dao/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * author: hufenggang
+ * email: hufenggang2019@gmail.com
+ * date: 2019/12/13 16:12
+ *
+ * 数据持久层Java代码
+ */
+package cn.zy.userprofile.realtime.dw.dao;
---
+++ realtime-dw-dao/src/main/scala/cn/zy/userprofile/realtime/dw/dao/package.scala
@@ -0,0 +1,12 @@
+package cn.zy.userprofile.realtime.dw
+
+/**
+ * author: hufenggang
+ * email: hufenggang2019@gmail.com
+ * date: 2019/12/13 16:13
+ *
+ * 数据持久层Scala代码
+ */
+package object dao {
+
+}
