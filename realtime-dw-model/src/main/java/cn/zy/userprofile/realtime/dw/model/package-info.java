79ca5871a5f9bae9e31ec224f7a680698051c628
hufenggang <michael.hu@jollycorp.com>
2019-12-13 16:18:16 +0800

    build model

---
+++ realtime-dw-model/src/main/java/cn/zy/userprofile/realtime/dw/model/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * author: hufenggang
+ * email: hufenggang2019@gmail.com
+ * date: 2019/12/13 16:16
+ *
+ * common model层Java代码
+ */
+package cn.zy.userprofile.realtime.dw.model;
---
+++ realtime-dw-model/src/main/scala/cn/zy/userprofile/realtime/dw/model/package.scala
@@ -0,0 +1,12 @@
+package cn.zy.userprofile.realtime.dw
+
+/**
+ * author: hufenggang
+ * email: hufenggang2019@gmail.com
+ * date: 2019/12/13 16:16
+ *
+ * common model层Scala代码
+ */
+package object model {
+
+}
