6bdd738e89a002374d9ab155e4bd3c7679bdfe50
hufenggang <michael.hu@jollycorp.com>
2019-12-13 17:44:39 +0800

    flink demo1

---
+++ realtime-dw-model/src/main/scala/cn/zy/userprofile/realtime/dw/model/test/package.scala
@@ -0,0 +1,12 @@
+package cn.zy.userprofile.realtime.dw.model
+
+/**
+ * author: hufenggang
+ * email: hufenggang2019@gmail.com
+ * date: 2019/12/13 17:13
+ *
+ * 开发相关Demo
+ */
+package object test {
+
+}
