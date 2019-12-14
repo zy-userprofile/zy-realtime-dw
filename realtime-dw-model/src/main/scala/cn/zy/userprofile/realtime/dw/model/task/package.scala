70435737c42877a239ed9438471fe925d021feef
hufenggang <michael.hu@jollycorp.com>
2019-12-14 11:10:32 +0800

    common model job package

---
+++ realtime-dw-model/src/main/scala/cn/zy/userprofile/realtime/dw/model/task/package.scala
@@ -0,0 +1,12 @@
+package cn.zy.userprofile.realtime.dw.model
+
+/**
+ * author: hufenggang
+ * email: hufenggang2019@gmail.com
+ * date: 2019/12/13 22:07
+ *
+ * 基于Flink作业实现common model模块
+ */
+package object task {
+
+}
