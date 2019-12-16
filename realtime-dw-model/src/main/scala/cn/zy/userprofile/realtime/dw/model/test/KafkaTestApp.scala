5d47aac3c69c37db7cd4c46f399dedf99c5cb905
hufenggang <michael.hu@jollycorp.com>
2019-12-16 11:33:18 +0800

    add flink-kafka demo

---
+++ realtime-dw-model/src/main/scala/cn/zy/userprofile/realtime/dw/model/test/KafkaTestApp.scala
@@ -0,0 +1,38 @@
+package cn.zy.userprofile.realtime.dw.model.test
+
+import java.util.Properties
+
+import cn.zy.userprofile.realtime.dw.common.constants.KafkaConstant
+import cn.zy.userprofile.realtime.dw.common.utils.ConfigScalaUtils
+import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.api.java.utils.ParameterTool
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
+
+/**
+ * author: hufenggang
+ * email: hufenggang2019@gmail.com
+ * date: 2019/12/16 10:51
+ *
+ * Flink消费Kafka数据 Demo
+ */
+object KafkaTestApp {
+
+    def main(args: Array[String]): Unit = {
+
+        val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+
+        val kafkaConfigMap = ConfigScalaUtils.getPropertiesMap(this.getClass, "/kafka.properties")
+
+        val properties = new Properties()
+        properties.setProperty("bootstrap.servers", kafkaConfigMap(KafkaConstant.KAFKA_CONFIG_BOOTSTRAP_SERVER_KEY))
+
+        val stream = env
+            .addSource(new FlinkKafkaConsumer[String]("stream1", new SimpleStringSchema(), properties))
+            .print()
+
+        env.execute("Scala KafkaTest Example")
+    }
+
+}
