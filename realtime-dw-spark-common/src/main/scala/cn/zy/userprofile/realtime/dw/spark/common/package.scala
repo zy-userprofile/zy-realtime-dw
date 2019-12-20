8634b8365ce17bd824ef0fa718a710e7cc80071c
hufenggang <hufenggang2019@gmai.com>
2019-12-20 21:14:49 +0800

    构建spark-common层

---
+++ realtime-dw-spark-common/src/main/scala/cn/zy/userprofile/realtime/dw/spark/common/package.scala
@@ -0,0 +1,12 @@
+package cn.zy.userprofile.realtime.dw.spark
+
+/**
+ * Author: hufenggang
+ * Email: hufenggang2019@gmail.com
+ * Date: 2019/12/20
+ *
+ * Spark公共模块
+ */
+package object common {
+
+}
