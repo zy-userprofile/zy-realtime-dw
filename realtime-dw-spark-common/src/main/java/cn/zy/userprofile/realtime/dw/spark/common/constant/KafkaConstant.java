691d572ac2e928c10eda590b460dd6e00f1a9d67
hufenggang <michael.hu@jollycorp.com>
2019-12-24 17:39:54 +0800

    update

--- realtime-dw-spark-common/src/main/java/cn/zy/userprofile/realtime/dw/spark/common/constant/KafkaConstant.java
+++ realtime-dw-spark-common/src/main/java/cn/zy/userprofile/realtime/dw/spark/common/constant/KafkaConstant.java
@@ -12,8 +12,10 @@ public interface KafkaConstant {
     String KAFKA_CONFIG_ZOOKEEPER_CONNECT_KEY = "kafka.zookeeper.connect";
 
     /****** Kafka Topic ******/
-    String KAFKA_TOPIC_PV = "countly_pv";
-    String KAFKA_TOPIC_EVENT = "countly_event";
-    String KAFKA_TOPIC_STREAM1 = "stream1";
-    String KAFKA_TOPIC_STREAM2 = "streams";
+    String KAFKA_TOPIC_PV = "kafka.topic.pv";
+    String KAFKA_TOPIC_EVENT = "kafka.topic.event";
+    String KAFKA_TOPIC_SESSION = "kafka.topic.session";
+    String KAFKA_TOPIC_METRICS = "kafka.topic.metrics";
+    String KAFKA_TOPIC_STREAM1 = "kafka.topic.stream1";
+    String KAFKA_TOPIC_STREAM2 = "kafka.topic.stream2";
 }
