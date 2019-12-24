52f9c50bb633f9d6355e3d1a2582f87b323c9687
hufenggang <michael.hu@jollycorp.com>
2019-12-24 17:36:25 +0800

    add monitor model

---
+++ realtime-dw-monitor/src/main/java/cn/zy/userprofile/monitor/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * author: hufenggang
+ * email: hufenggang2019@gmail.com
+ * date: 2019/12/24 14:26
+ */
+package cn.zy.userprofile.monitor;
---
+++ realtime-dw-monitor/src/main/scala/cn/zy/userprofile/monitor/package.scala
@@ -0,0 +1,10 @@
+package cn.zy.userprofile
+
+/**
+ * author: hufenggang
+ * email: hufenggang2019@gmail.com
+ * date: 2019/12/24 14:27
+ */
+package object monitor {
+
+}
